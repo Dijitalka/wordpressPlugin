cc96c92a0cf772d902052b3b670dff6dc1a72225
tunahansr <tunaha.srn@gmail.com>
2022-12-16 22:48:46 +0300

    coreSettings

--- pages/index.php
+++ pages/index.php
@@ -4,10 +4,10 @@ function coreSettings()
 {
     ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <div class="container text-center">
+        <div class="container text-center p-5">
             <div class="row">
                 <div class="col-12">
-                    <img class="col-12" src="../assets/images/logo.png">
+                    <img class="col-12" src="<?= __FILE__?>assets/images/logo.png">
                 </div>
             </div>
             <div class="row">
