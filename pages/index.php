a48f36dcea8f4862afc8fe2af8ccd55af5e617ce
tunahansr <tunaha.srn@gmail.com>
2022-12-16 22:55:32 +0300

    coreSettings

--- pages/index.php
+++ pages/index.php
@@ -4,18 +4,25 @@ function coreSettings()
 {
     ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <div class="container text-center p-5" style="background-color:black;">
+        <div class="container text-center p-5 text-center" style="background-color:#F4ED7D;">
             <div class="row">
-                <div class="col-12" style="text-align: center;">
+                <div class="col-12">
                     <img src="https://dijitalka.com/wp-content/uploads/2022/05/3-300x114.png">
                 </div>
             </div>
             <div class="row">
                 <div class="col-6">
-                    Column
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
                 </div>
                 <div class="col-6">
-                    Column
+
                 </div>
             </div>
         </div>
