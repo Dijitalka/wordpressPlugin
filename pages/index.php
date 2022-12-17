e9b2819b8523ae2e719b88338aa0f8e15d4ec9ab
tunahansr <tunaha.srn@gmail.com>
2022-12-17 11:04:42 +0300

    coreSettings

--- pages/index.php
+++ pages/index.php
@@ -21,19 +21,6 @@ function coreSettings()
                     <img src="https://dijitalka.com/wp-content/uploads/2022/05/3-300x114.png">
                 </div>
             </div>
-            <div class="row">
-                <div class="col-6">
-                    <div class="mb-3">
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Dijitalka sistemlerinden aldığınız token değeri">
-                    </div>
-                    <div class="mb-3">
-                        <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="Dijitalka sistemlerine bağlanmamız için url">
-                    </div>
-                </div>
-                <div class="col-6">
-
-                </div>
-            </div>
         </div>
     <?php
 }
