68e5d1d9327fb3003120a8c96d08de0db55eefdf
tunahansr <tunaha.srn@gmail.com>
2022-12-17 19:47:46 +0300

    coreSettings

--- pages/index.php
+++ pages/index.php
@@ -23,15 +23,15 @@ function coreSettings()
             </div>
             <div class="row">
                 <div class="col-6">
-                    <div class="input-group flex-nowrap">
+                    <div class="input-group flex-nowrap ">
                         <span class="input-group-text" id="addon-wrapping">Site URL</span>
-                        <input type="text" class="form-control" name="siteURL" placeholder="Site URL" aria-label="Username" aria-describedby="addon-wrapping">
+                        <input type="text" class="form-control transparent-input" name="siteURL" placeholder="Site URL" aria-label="Username" aria-describedby="addon-wrapping">
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="input-group flex-nowrap">
                         <span class="input-group-text" id="addon-wrapping">TOKEN</span>
-                        <input type="text" class="form-control" placeholder="TOKEN" name="token" aria-label="Username" aria-describedby="addon-wrapping">
+                        <input type="text" class="form-control transparent-input" placeholder="TOKEN" name="token" aria-label="Username" aria-describedby="addon-wrapping">
                     </div>
                 </div>
             </div>
