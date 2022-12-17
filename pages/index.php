744145115ca7f1acf16927d70a268162ae332366
tunahansr <tunaha.srn@gmail.com>
2022-12-17 10:44:17 +0300

    coreSettings

--- pages/index.php
+++ pages/index.php
@@ -4,7 +4,13 @@ function coreSettings()
 {
     ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <div class="container text-center p-5 text-center" style="background-color:#F4ED7D;">
+        <style>
+            body
+            {
+                background-color: black ; !important;
+            }
+        </style>
+        <div class="container text-center p-5 text-center">
             <div class="row">
                 <div class="col-12">
                     <img src="https://dijitalka.com/wp-content/uploads/2022/05/3-300x114.png">
